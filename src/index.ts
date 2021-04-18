9606ed66a2530ad5ef82d9691682f1865bdc4fb4
Cole <colemag383@gmail.com>
2021-04-19 02:13:21 +1200

    Submitting search box will go to the first listed anime

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,6 @@ import * as malApi from './malApi';
 
 const graph: HTMLElement = document.querySelector("#mermaid-graph")
 const table: HTMLElement = document.querySelector("#work-list")
-const searchbox: HTMLElement = document.querySelector("input#anime-id")
-const searchResults: HTMLElement = document.querySelector("#search-results")
 
 window.onload = () => {
   mermaid.initialize({
@@ -17,7 +15,7 @@ window.onload = () => {
 
   const workId = new URLSearchParams(window.location.search).get("work_id")
   if (workId) {
-    submitListener(workId);
+    graphWork(workId);
   }
 
   table.addEventListener('mouseover', sidebar.mouseOverSidebar);
@@ -26,10 +24,20 @@ window.onload = () => {
   graph.addEventListener('mouseover', sidebar.mouseOverGraph);
   graph.addEventListener('mouseout', sidebar.mouseOutGraph);
 
+  const searchbox: HTMLElement = document.querySelector("input#anime-id")
+  const searchResults: HTMLElement = document.querySelector("#search-results")
   searchbox.addEventListener('input', e => sidebar.search(e, searchResults));
+
+  document.querySelector("form").addEventListener('submit', e => {
+    e.preventDefault();
+    if (searchResults.hasChildNodes) {
+      // ul>li>a
+      (searchResults.childNodes[0].childNodes[0] as HTMLLinkElement).click()
+    }
+  })
 }
 
-async function submitListener(workId: string) {
+async function graphWork(workId: string) {
   malApi.startRecurse(workId,
     c => {
       graph.innerHTML = renderGraph(c);
